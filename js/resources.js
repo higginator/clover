dbc000596c3025e1c3ab5dbabe40f071dcbd68e3
Jingyi Li <soundtracknoon@gmail.com>
2015-12-06 14:16:11 -0800

    start fresh

---
+++ js/resources.js
@@ -0,0 +1,34 @@
+game.resources = [
+
+	 //level tileset
+    {name: "branches-01",  type:"image", src: "data/map/branches-01.png"},
+    {name: "branches-02",  type:"image", src: "data/map/branches-02.png"},
+    {name: "branches-03",  type:"image", src: "data/map/branches-03.png"},
+   
+    // // our metatiles
+    // {name: "collision-tiles",  type:"image", src: "data/map/collision-tiles.png"},
+
+    //background
+    {name: "bg",  type:"image", src: "data/map/bg.png"},
+    
+    // //you spritesheet
+    // {name: "self-walk-short",  type:"image", src: "data/img/self-walk-short.png"},
+    // {name: "summer-self-walk",  type:"image", src: "data/img/summer-self-walk.png"},
+    // {name: "spring-self-walk",  type:"image", src: "data/img/spring-self-walk.png"},
+    // {name: "you-as-npc-walk",  type:"image", src: "data/img/you-as-npc-walk.png"},
+    // //font
+    // {name: "32x32_font", type:"image", src: "data/img/font/idek.png"},
+
+    // Maps
+    {name: "level-01", type: "tmx", src: "data/level-01.tmx"},
+	
+    // Background Music
+	// {name: "fall", type: "audio", src: "data/bgm/"},
+    
+    // Title Screen
+    // {name: "title_screen", type: "image", src: "data/img/gui/title_screen.png"},
+
+	
+    // Cling
+	// {name: "cling", type: "audio", src: "data/sfx/"},
+];
