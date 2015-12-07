35d4d1a89cfe8bbbe0a3ef0a5ac1a3966c145e38
Ryan Higgins <ryanhigs@gmail.com>
2015-12-07 01:18:58 -0800

    works as a node.js server now. also has sockets working

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var express = require('express');
+var path = require('path');
+var app = express();  // Module to control application life.
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.use("/css", express.static(path.join(__dirname, '/css')));
+app.use("/lib", express.static(path.join(__dirname, '/lib')));
+app.use("/js", express.static(path.join(__dirname, '/js')));
+app.use("/data", express.static(path.join(__dirname, '/data')));
+
+/*
+//var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
 // Report crashes to our server.
 //require('crash-reporter').start();
@@ -7,6 +19,7 @@ var BrowserWindow = require('browser-window');  // Module to create native brows
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the javascript object is GCed.
 var mainWindow = null;
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -21,7 +34,7 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 960, height: 660});
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadUrl('file://' + __dirname + '/indexs.html');
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
@@ -30,4 +43,22 @@ app.on('ready', function() {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
+});*/
+
+app.get('/', function(request, response) {
+  response.sendfile('index.html', {root: __dirname});
 });
+
+app.get('/red', function(request, response) {
+  response.send('<h1>red sent to game</h1>');
+  io.emit('red');
+});
+
+io.on('connection', function(socket) {
+  console.log('new user connected');
+})
+
+http.listen(3000, function() {
+  console.log('app running on port ' + 3000);
+})
+
