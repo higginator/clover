a45de3d9e9ab203e53444617c92ea18eab257f92
Ryan Higgins <ryanhigs@gmail.com>
2015-12-07 03:45:08 -0800

    port switch

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var app = express();  // Module to control application life.
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 5000));
+
 
 app.use("/css", express.static(path.join(__dirname, '/css')));
 app.use("/lib", express.static(path.join(__dirname, '/lib')));
@@ -45,9 +47,6 @@ app.on('ready', function() {
   });
 });*/
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
 
 app.get('/', function(request, response) {
   response.sendfile('index.html', {root: __dirname});
@@ -77,7 +76,6 @@ http.listen(3000, function() {
   console.log('app running on port ' + 3000);
 })*/
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
+http.listen(app.get('port'), function() {
+  console.log('app running on port ' + app.get('port'));
+})
